ddf54f6d705dfc01ca289f25c8ea158b102ff66d
IreshaWarani <ireshawarani0@gmail.com>
2025-09-13 21:03:04 +0530

    init commit

---
+++ src/LW_02/Q7.java
@@ -0,0 +1,12 @@
+package LW_02;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Q7 {
+    public static void main(String[] args) {
+        LocalDate today = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EEEE, MMMM d, yyyy");
+        System.out.println(today.format(formatter));
+    }
+}
